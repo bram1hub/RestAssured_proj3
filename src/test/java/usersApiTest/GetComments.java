b2e73a67e928913edf570d60decc1d59e4aa18bf
balaramhub1 <balaram1985behera@gmail.com>
2022-09-04 21:12:49 +0530

    new test class for get comments

---
+++ src/test/java/usersApiTest/GetComments.java
@@ -0,0 +1,11 @@
+package usersApiTest;
+
+import org.testng.annotations.Test;
+
+import baseApiTest.BaseTest;
+
+public class GetComments extends BaseTest {
+	@Test
+	public void verifyGETComments() {
+	}
+}
